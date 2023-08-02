ef7a48ff9a2c30f1c022de40c8377d2454fb6925
LevonteyA <136347199+LevonteyA@users.noreply.github.com>
2023-08-03 00:11:03 +0300

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AppOnlineShopMConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_online_shop_m'
+    verbose_name = 'Объявления'
